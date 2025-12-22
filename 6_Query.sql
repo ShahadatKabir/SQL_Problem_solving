a4402b16e2532e461432a85393724d42bca0763c
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2025-12-22 23:39:36 +0600

    Add comment to 6_Query.sql for clarity
    Added a comment explaining the query and the STATION table structure.

--- 6_Query.sql
+++ 6_Query.sql
@@ -1,1 +1,6 @@
+/*Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.*/
 select  CITY,STATE  From STATION where LAT_N>0 and LONG_W>0;
