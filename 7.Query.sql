30a870964146daee6d97a6d13b0f5a9ac266daa6
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2025-12-23 23:54:16 +0600

    Remove duplicate query for even ID cities

--- 7.Query.sql
+++ 7.Query.sql
@@ -1,2 +1,4 @@
 /*Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.*/
 select distinct city from station where mod(ID,2)=0;
+
+select distinct city from station where ID%2=0;
