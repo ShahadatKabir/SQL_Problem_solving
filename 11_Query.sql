44c87e5872f3ef1cf6603f27ebd155c865f316d8
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2025-12-30 01:07:20 +0600

    Update 11_Query.sql

--- 11_Query.sql
+++ 11_Query.sql
@@ -1,1 +1,10 @@
-select * from station;
+/*Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.*/
+
+select distinct city
+from station
+where city like '%a'
+or city like '%e'
+or city like '%i'
+or city like '%o'
+or city like '%u'
+order by city;
