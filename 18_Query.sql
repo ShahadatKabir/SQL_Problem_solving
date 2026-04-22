5f1b3c1c13526b26e8b329979a2b33e9bb64c33d
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-04-22 18:27:10 +0600

    Create 18_Query.sql

---
+++ 18_Query.sql
@@ -0,0 +1,6 @@
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+--Only Ashley, Julia, and Belvet have Marks > . If you look at the last three characters of each of their names, there are no duplicates and 'ley' < 'lia' < 'vet'.
+select name 
+from students
+where mark >75
+orer by substr (name, length(name),-2,3) ,id;
